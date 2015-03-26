bab3d8e8e12d8b0d03e9814c666fa7d292d15592
efgallegos <efgallegos@gmail.com>
2015-03-25 23:19:31 -0300

    update stc config file

--- bankers/stc/config_entries_stc.py
+++ bankers/stc/config_entries_stc.py
@@ -4,6 +4,51 @@ xmls_entry = {
      '': '',
      }
 
+
+"""
+<Data Name="ADDR_State">AL</Data>
+<Data Name="ADDR_State_itmtxt">ALABAMA</Data>
+<Data Name="ADDR_State_itmval">AL</Data>
+<Data Name="AGENT_ADDR_State">PA</Data>
+<Data Name="AGENT_ADDR_State_itmtxt">PENNSYLVANIA</Data>
+<Data Name="AGENT_FullAddress">123 Agent St Exton, AK 11111</Data>
+<Data Name="AGENT_pdf_FullAddress">Address Line 1, Address 2, Exton, PA, 111111111</Data>
+<Data Name="AGENT2_ADDR_State">AK</Data>
+<Data Name="AGENT2_ADDR_State_itmtxt">ALASKA</Data>
+<Data Name="AGENT2_ADDR_State_itmval">AK</Data>
+<Data Name="AGENT2_FullAddress">123 Second St Exton, AK 11111</Data>
+<Data Name="BILLING_ADDR_State">AK</Data>
+<Data Name="BILLING_ADDR_State_itmtxt">ALASKA</Data>
+<Data Name="BILLING_ADDR_State_itmval">AK</Data>
+<Data Name="EFT_State">AL</Data>
+<Data Name="EFT_State_itmtxt">ALABAMA</Data>
+<Data Name="EFT_State_itmval">AL</Data>
+<Data Name="pdfAGENT_FullNameAddress">Agent Name 123 Agent St Exton, AK 11111</Data>
+<Data Name="PIADDR_State">AL</Data>
+<Data Name="PIFullAddress">123 Main St Exton, AL 11111</Data>
+
+<Data Name="PIFullName">STC X GR-N560 Sr</Data>
+<Data Name="pdfReversePIName">GR-N560, STC X</Data>
+<Data Name="PILastName">GR-N560</Data>  
+<Data Name="PIFirstName">STC</Data>
+
+<Data Name="PIJurisdiction">AL</Data>
+<Data Name="PIJurisdiction_itmtxt">Alabama</Data>
+<Data Name="PIJurisdiction_itmval">AL</Data>
+<Data Name="ProducerAddressState">PA</Data>
+<Data Name="ProducerAddressStateTC">PA</Data>
+<Data Name="ProducerCityStateZip">Exton, PA 11111-1111</Data>
+<Data Name="State">AL</Data>
+<Data Name="State_itmtxt">Alabama</Data>
+<Data Name="State_itmval">AL</Data>
+
+<Data Name="TPState">AL</Data>
+<Data Name="TPState_itmtxt">ALABAMA</Data>
+<Data Name="TPState_itmval">AL</Data>
+
+<Data Name="CaseDescription">State=AL</Data>
+"""
+
 # Product's valid states
 
 states = {
@@ -13,10 +58,10 @@ states = {
     'AR': 'Arkansas',
     'CA': 'California',
     'CO': 'Colorado',
-    'CT': 'Connecticut',
+    # 'CT': 'Connecticut',
     'DE': 'Delaware',
     'DC': 'District of Columbia',
-    'FL': 'Florida',
+    # 'FL': 'Florida',
     'GA': 'Georgia',
     'HI': 'Hawaii',
     'ID': 'Idaho',
@@ -28,20 +73,20 @@ states = {
     'LA': 'Louisiana',
     'ME': 'Maine',
     'MD': 'Maryland',
-    'MA': 'Massachusetts',
+    # 'MA': 'Massachusetts',
     'MI': 'Michigan',
-    'MN': 'Minnesota',
+    # 'MN': 'Minnesota',
     'MS': 'Mississippi',
     'MO': 'Missouri',
     'MT': 'Montana',
     'NE': 'Nebraska',
     'NV': 'Nevada',
     'NH': 'New Hampshire',
-    'NJ': 'New Jersey',
+    # 'NJ': 'New Jersey',
     'NM': 'New Mexico',
-    'NY': 'New York',
+    # 'NY': 'New York',
     'NC': 'North Carolina',
-    'ND': 'North Dakota',
+    # 'ND': 'North Dakota',
     'OH': 'Ohio',
     'OK': 'Oklahoma',
     'OR': 'Oregon',
@@ -49,23 +94,29 @@ states = {
     # 'PR': 'Puerto Rico',
     'RI': 'Rhode Island',
     'SC': 'South Carolina',
-    'SD': 'South Dakota',
+    # 'SD': 'South Dakota',
     'TN': 'Tennessee',
-    'TX': 'Texas',
+    # 'TX': 'Texas',
     'UT': 'Utah',
-    'VT': 'Vermont',
+    # 'VT': 'Vermont',
     'VA': 'Virginia',
-    'WA': 'Washington',
+    # 'WA': 'Washington',
     'WV': 'West Virginia',
-    'WI': 'Wisconsin',
+    # 'WI': 'Wisconsin',
     'WY': 'Wyoming'
     }
 
 # Product's valid plans
 
 plans = {
-    '': {'full_name': '',
-         'file_name': ' '}
+    'GR-N560': {'full_name': 'GR-N560',
+                'file_name': 'GR-N560'},
+    'GR-N565': {'full_name': 'GR-N565',
+                'file_name': 'GR-N565'},
+    'GR-N320': {'full_name': 'GR-N320',
+                'file_name': 'GR-N320'},
+    'GR-N325': {'full_name': 'GR-N325',
+                'file_name': 'GR-N325'}
     }
 
 # Product configuration dictionary
