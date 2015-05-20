a7053905019de6667aed0b541b5ae864b803f48e
efgallegos <efgallegos@gmail.com>
2015-05-19 22:56:06 -0300

    new carrier config file for Bankers

---
+++ config_entries.py
@@ -0,0 +1,32 @@
+"""Dictionaries Configuration"""
+
+import platform
+from igo.igo_config_entries import igo_config
+from bankers.config_bankers import bankers_config
+
+def get_base_path():
+    if platform.system() == "Darwin":
+        return '/Users/efgallegos/Dropbox/Automation/'
+    return 'C:\\zz_EFG\\Dropbox\\Automation\\'
+
+def get_path_separator():
+    if platform.system() == "Darwin":
+        return'/'
+    return '\\'
+
+config_values = {
+    'base_path': get_base_path(),
+    'os_path_separator': get_path_separator(),
+    'igo_common': igo_config,
+    'submitted_xmls': 'SUBMITTED_XMLS',
+    'carriers':['bankers'],
+    'bankers':bankers_config,
+    }
+
+def load():
+    output_file_path = get_base_path() + 'config_entries.txt'
+    with open(output_file_path,'r') as f:
+        for line in f:
+            new_entry = line.split(',')
+            config_values[new_entry[0]] = new_entry[1]
+load()
